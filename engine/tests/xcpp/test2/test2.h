f6ab55c34b7f6515b7ee5fab95d5006c1b33f173
hsz <hszsoftware@qq.com>
2025-04-06 16:50:58 +0800

    test: update test_xcpp

--- engine/tests/xcpp/test2/test2.h
+++ engine/tests/xcpp/test2/test2.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <cstdint>
+
 namespace TestXcpp {
 
 enum class
 [[clang::annotate("__reflect__")]]
-TestEnum {
+TestEnum : std::uint8_t {
     kConstant1,
     kConstant2,
     kConstant3
 };
+
+[[clang::annotate("__reflect__")]]
+void globalTestFunction2();
 
 } // namespace TestXcpp
